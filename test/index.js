7fe4e552230c761fb67083970e8ce4753aded446
Josh Taylor <joshrtay@gmail.com>
2015-12-16 10:31:34 -0800

    remove es6 syntax dep test

--- test/index.js
+++ test/index.js
@@ -10,13 +10,21 @@ var test = require('tape')
  */
 
 test('should work', function (t) {
-  var reducer = reduceKey(() => 'test', (state, value) => value)
+  var reducer = reduceKey(function () {
+    return 'test'
+  }, function (state, value) {
+    return value
+  })
   t.equal(reducer({test: 1}, 2).test, 2)
   t.end()
 })
 
 test('should not clone if nothing was changed', function (t) {
-  var reducer = reduceKey(() => 'test', (state, value) => value === 3 ? value : state)
+  var reducer = reduceKey(function () {
+    return 'test'
+  }, function (state, value) {
+    return value === 3 ? value : state
+  })
   var test = {test: 1}
 
   t.equal(reducer(test, 2), test)
